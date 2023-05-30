f4b1130bc66be2681cef255bb9e88ad5c2ab008a
Fidanbaratli1 <fidanbaratli@gmail.com>
2023-05-30 08:28:19 +0400

    Add project files.

---
+++ Ebusinesstemplate/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Ebusinesstemplate.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ebusinesstemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Ebusinesstemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ebusinesstemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
